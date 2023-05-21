5aace11dd6557f5bc6b2ad22b18c7b76a505e523
StunnaDawg <jonsonallen9@gmail.com>
2023-05-21 19:33:55 -0300

    model base

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -2,6 +2,20 @@ const {Schema, model} = require('mongoose');
 
 const thoughtsSchema = new Schema(
     {
-        
+        thoughtText: {
+            type: String, 
+            required: true, 
+            minLength: 1, 
+            maxLength: 280
+        }, 
+        createdAt: {
+            type: Date, 
+            default: Date.now()
+        }, 
+        username: {
+            type: String, 
+            required: true
+        }, 
+        reactions: [reactionsSchema]
     }
 )
--- models/Users.js
+++ models/Users.js
@@ -1,21 +1,33 @@
 const {Schema, model} = require('mongoose');
 const Thoughts = require('./Thoughts')
 
-function validateEmail(email)
+function validateEmail(email) {
+    let regex = '/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/'
+    regex.test(email)
+}
 
 const usersSchema = new Schema(
     {
        username: {
         type: String,
         unique: true,
-        required: true,
+        required: 'username is required',
         trim: true
        },
        email: {
         type: String,
         unique: true,
-        required: true,
-        trim: true
-       }
-    }
+        required: 'email is required',
+        validate: [validateEmail, 'Enter a vaild email'],
+        match: ['/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/']
+       },
+       thoughts: [Thoughts._id],
+       friends: [this]
+    },
+    {
+        toJSON: {
+          virtuals: true,
+        },
+        id: false,
+      }
 )
