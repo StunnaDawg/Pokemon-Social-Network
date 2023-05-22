db6a0e1026beacce4d8b08ec0d83dd0a783470ad
StunnaDawg <jonsonallen9@gmail.com>
2023-05-22 17:20:10 -0300

    get id/create controllers

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -1,4 +1,4 @@
-const { Thoughts } = require("../models");
+const { Thoughts, User } = require("../models");
 
 module.exports = {
     async getThoughts(req, res) {
@@ -8,5 +8,39 @@ module.exports = {
       } catch (err) {
         res.status(500).json(err);
       }
-    }
+    }, 
+    async getSingleThought(req, res) {
+        try {
+          const singleThought = await Thoughts.findOne({ _id: req.params.thoughtId });
+    
+          if (!singleThought) {
+            return res.status(404).json({ message: 'No thought with that ID' });
+          }
+    
+          res.json(singleThought);
+        } catch (err) {
+          res.status(500).json(err)
+        }
+      },
+      async createThought(req, res) {
+        try {
+          const thought = await Thoughts.create(req.body);
+          const user = await User.findOneAndUpdate(
+            { _id: req.body.userId },
+            { $addToSet: { thoughts: thought._id } },
+            { new: true }
+          );
+    
+          if (!user) {
+            return res
+              .status(404)
+              .json({ message: 'No user to attach to the created thought' });
+          }
+    
+          res.json('Created the post 🎉');
+        } catch (err) {
+          console.log(err);
+          res.status(500).json(err);
+        }
+      },
 }
